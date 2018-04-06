f254b13d82d07b27814732dc807cd00f06f3b37e
geekecuador <dapl@me.com>
2018-04-06 00:19:26 -0500

    ok

--- carrera/admin.py
+++ carrera/admin.py
@@ -5,7 +5,7 @@ admin.site.register(Categoria)
 
 @admin.register(Inscripcion)
 class InscripcionAdmin(admin.ModelAdmin):
-  search_fields = ['apellidos',]
+  search_fields = ['apellidos','cedula']
   list_display = ('nombres','apellidos','categoria','Pago')
   list_display = ('nombres','apellidos','categoria','Pago','cedula')
   list_filter = ('categoria','Pago')
